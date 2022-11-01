fda2bfa73c4fa1a141679a0aec5b660e6f848dd2
beatsnob <110766370+beatsnob@users.noreply.github.com>
2022-11-01 22:51:39 +1100

    added queries and marshmallow

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+app.config ['JSON_SORT_KEYS'] = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
 
 db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -16,6 +20,11 @@ class Card(db.Model):
     date = db.Column(db.Date)
     status = db.Column(db.String)
     priority = db.Column(db.String)
+
+class CardSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'description', 'status', 'priority', 'date')
+        ordered = True
 
 #define a custom CLI (terminal) command
 @app.cli.command('create')
@@ -64,6 +73,38 @@ def seed_db():
     db.session.add_all(cards)
     db.session.commit()
     print('Tables seeded')
+
+@app.route('/cards/')
+def all_cards():
+    #commented section is the legacy way to query the ORM
+    #select * from cards; psql equivalent to below
+    #cards = Card.query.all()
+    #print(cards)
+    #the following is the new way
+    stmt = db.select(Card).order_by(Card.priority.desc(), Card.title)
+    cards = db.session.scalars(stmt)
+    return CardSchema(many=True).dump(cards)
+    #for card in cards:
+    #    print(card.__dict__)
+
+
+@app.cli.command('first_card')
+def first_card():
+    #commented section is the legacy way to query the ORM
+    #select * from cards limit 1; psql equivalent to below
+#   cards = Card.query.first()
+#   print(cards)
+    #the following is the new way
+    stmt = db.select(Card).limit(1)
+    card = db.session.scalar(stmt)
+    print(card.__dict__)
+
+@app.cli.command('count_ongoing')
+def count_ongoing():
+    stmt = db.select(db.func.count()).select_from(Card).filter_by(status='Ongoing')
+    cards = db.session.scalar(stmt)
+    print(cards)
+
 
 @app.route('/')
 def index():
