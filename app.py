abbb75c786d6aef409b9d2e8e4d506fa1ab488cc
beatsnob <110766370+beatsnob@users.noreply.github.com>
2022-11-02 16:52:43 +1100

    added authentication

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
+from enum import unique
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config ['JSON_SORT_KEYS'] = False
+app.config['JSON_SORT_KEYS'] = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
 
 db = SQLAlchemy(app)
-
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'is_admin')
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -39,6 +53,21 @@ def drop_db():
 
 @app.cli.command('seed')
 def seed_db():
+
+    users = [
+        User(
+            email = 'admin@spam.com',
+            password = bcrypt.generate_password_hash('eggs'),
+            is_admin = True
+        ),
+        User(
+            name = 'John Cleese',
+            email = 'someone@spam.com',
+            password = bcrypt.generate_password_hash('12345')
+
+        )
+    ]
+    
     cards = [
         Card(
             title = 'Start the project',
@@ -71,6 +100,7 @@ def seed_db():
     ]
 
     db.session.add_all(cards)
+    db.session.add_all(users)
     db.session.commit()
     print('Tables seeded')
 
